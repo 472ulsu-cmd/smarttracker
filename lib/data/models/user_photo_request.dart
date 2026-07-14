60b0a0fa3a51a14fea0755a43102c4e6367203bd
Sobjanin Anton <472@ul.su>
2026-07-14 15:30:13 +0300

    feat: add UserPhotoRequest freezed model

---
+++ lib/data/models/user_photo_request.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_photo_request.freezed.dart';
+part 'user_photo_request.g.dart';
+
+/// Тело запроса загрузки фото пользователя (`POST /user/photo`).
+@freezed
+class UserPhotoRequest with _$UserPhotoRequest {
+  const factory UserPhotoRequest({
+    String? avatar,
+  }) = _UserPhotoRequest;
+
+  factory UserPhotoRequest.fromJson(Map<String, dynamic> json) =>
+      _$UserPhotoRequestFromJson(json);
+}
